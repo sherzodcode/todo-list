dfe4960122bb1b72e5c09d9e3ff5df6a1767a27e
sherzodcode <sherzodcode@gmail.com>
2023-04-19 13:05:30 +0500

    Changed

--- out/server.js
+++ out/server.js
@@ -9,14 +9,17 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const router_1 = __importDefault(require("./api/router"));
 const app = (0, express_1.default)();
-app.use(express.static('public'))
+
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use('/api', router_1.default);
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+app.get('/' ,(req,res) => {
+    return res.status(200).json({
+        message: "Worked"
+    })
 })
+
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Serever is running on port: ${port}`);
